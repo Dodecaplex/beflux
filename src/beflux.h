628cf648d56dec7723e0c4bf2eeebcadc08c69ed
Dodecaplex <adchiodo@gmail.com>
2014-12-12 23:12:01 -0800

    Updated beflux.h structure.

--- src/beflux.h
+++ src/beflux.h
@@ -4,60 +4,90 @@
 
 #pragma once
 
-static const uint BLOCK_W = 16;
-static const uint BLOCK_H = 16;
-static const uint BLOCK_MAX = BLOCK_W * BLOCK_H;
-
-static const uint BINDING_MAX = 256;
+#include "util.h"
 
 namespace Dodecaplex {
 
 class Beflux {
 public:
+  static const uint BLOCK_W = 16;
+  static const uint BLOCK_H = 16;
+  static const uint BLOCK_MAX = BLOCK_W * BLOCK_H;
+
+  static const int CURSOR_W = -1;
+  static const int CURSOR_E = +1;
+  static const int CURSOR_N = -BLOCK_W;
+  static const int CURSOR_S = +BLOCK_W;
+  static const int CURSOR_STOP = 0;
+
+  static enum {
+    PROGRAM = 0,
+    MEMORY,
+    INPUT,
+    OUTPUT,
+    SEGMENT_MAX
+  } segment_id;
+
+  static const uint BINDING_MAX = 256;
+
   class Block {
   public:
     Block(void);
-    Block(const byte *src);
+    Block(const byte * const src, uint count);
     Block(const char * const filename);
 
-    load(const char * const filename);
-    save(const char * const filename) const;
-    clear(void);
+    // TODO: read / write from rect in src
+    void clear(void);
+    void read(const byte * const src, uint count);
+    void write(byte * const dst, uint count) const;
+    void load(const char * const filename);
+    void save(const char * const filename) const;
+
+    void set(byte value);
+    void set(uint s, byte value);
+    void set(uint x, uint y, byte value);
+    byte get(void) const;
+    byte get(uint s) const;
+    byte get(uint x, uint y) const;
+
+    void push(byte x);
+    byte pop(void);
+
+    void cursor_set_position(uint s);
+    void cursor_set_velocity(int ds);
+    uint cursor_get_position(void) const;
+    int  cursor_get_velocity(void) const;
+    void cursor_advance(void);
+    void cursor_backtrack(void);
+    void cursor_reset(void);
+
+    byte data[BLOCK_MAX];
+    bool row_wrap;
 
   private:
-    byte data_[BLOCK_MAX];
+    class Cursor_ {
+    public:
+      Cursor_(uint i_s=0, int i_ds=CURSOR_STOP) : s(i_s), ds(i_ds) {}
+      uint s;
+      int ds;
+    } cursor_;
+
   };
 
-  void set_program(const Block &src);
-  void set_memory(const Block &src);
-  void set_input(const Block &src);
-  void set_output(const Block &src);
+  Beflux(void);
 
-  Block &program(void) const;
-  Block &memory(void) const;
-  Block &input(void) const;
-  Block &output(void) const;
+  Block *program(void);
+  Block *memory(void);
+  Block *input(void);
+  Block *output(void);
 
   byte run(void);
 
-  void bind(uint index, void (*binding)(Beflux *));
-  void unbind(uint index);
+  void bind(uint index, void (*binding)(Beflux *)=nullptr);
+  void hook(void (*pre)(Beflux *)=nullptr, void (*post)(Beflux *)=nullptr);
 
 private:
-  Block program_;
-  Block memory_;
-  Block input_;
-  Block output_;
-
-  struct Cursor_ {
-    ulong x, y;
-    ulong dx, dy;
-  };
-
-  Cursor program_cursor_;
-  Cursor memory_cursor_;
-  Cursor input_cursor_;
-  Cursor output_cursor_;
+  Block segment_[SEGMENT_MAX];
 
   bool active_;
   byte status_;
@@ -67,11 +97,14 @@ private:
   byte value_;
   byte t_major_;
   byte t_minor_;
+  byte loop_counter_;
 
   void update_(void);
   void eval_(byte op);
 
   void (*bindings_[BINDING_MAX])(Beflux *);
-}
+  void (*pre_update_)(Beflux *);
+  void (*post_update_)(Beflux *);
+};
 
 } // namespace Dodecaplex
